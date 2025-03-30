48191ff76a84e50fbabf64e90899513910e1a31b
Zachary Walravens <zacharywalravens@Zacharys-MacBook-Air.local>
2025-03-30 15:12:33 -0700

    update title and icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Zach's Chess Game</title>
+        <link rel="icon" type="image/png" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
